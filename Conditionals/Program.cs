b9ba073204fa8d5f47ae14ff8d7737e088c27302
Ryan LeBlanc <ryanleb2003@gmail.com>
2023-09-25 10:22:49 -0300

    weapon list

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace Conditionals
         // declarations
         static int health;
         static int lives;
-
+        static int weapon;
 
 
         static void Main(string[] args)
@@ -23,7 +24,7 @@ namespace Conditionals
             // initialization
             health = 100;
             lives = 3;
-
+            weapon = 0;
 
 
             // take damage
@@ -34,7 +35,7 @@ namespace Conditionals
             ShowHUD();
             TakeDMG(50);
             ShowHUD();
-            TakeDMG(25);
+            Heal(25);
             ShowHUD();
 
             Console.WriteLine();
@@ -48,6 +49,50 @@ namespace Conditionals
         {
             Console.WriteLine("Health: " + health);
             Console.WriteLine("Lives: " + lives);
+        }
+        
+        static void Heal(int healed)
+        {
+            health = health + healed;
+        }
+        
+        
+        static void weaponSelected(int weaponPickedup)
+        {
+
+            weapon = weapon + weaponPickedup;
+            
+            
+            if (weapon == 0)
+            {
+                Console.WriteLine("Pistol equipped");
+            }
+
+            if (weapon == 1)
+            {
+                Console.WriteLine("shotgun equipped");
+            }
+
+            if (weapon == 2)
+            {
+                Console.WriteLine("spreader equipped");
+            }
+        
+            if (weapon == 3)
+            {
+                Console.WriteLine("laser equipped");
+            }
+            
+            if (weapon == 4)
+            {
+                Console.WriteLine("sniper equipped");
+            }
+
+            if (weapon == 5)
+            {
+                Console.WriteLine("bfg equipped (big friendly giant)");
+            }
+        
         }
         
         static void TakeDMG(int damage)
